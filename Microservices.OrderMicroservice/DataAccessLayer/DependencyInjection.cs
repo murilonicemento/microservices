4ccb2da87601a9c7f50b3dfbfb637a6c1f7d1bf8
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-13 15:53:13 -0300

    feat: add packages and define boilerplate code

---
+++ Microservices.OrderMicroservice/DataAccessLayer/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccessLayer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configurationF)
+    {
+        return services;
+    }
+}
