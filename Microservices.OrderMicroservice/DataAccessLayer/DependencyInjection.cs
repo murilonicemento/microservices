8422e2d25596e96e4c3a09ae50d7495caee6e4c6
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-13 17:04:24 -0300

    feat: add mongo db

--- Microservices.OrderMicroservice/DataAccessLayer/DependencyInjection.cs
+++ Microservices.OrderMicroservice/DataAccessLayer/DependencyInjection.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace DataAccessLayer;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configurationF)
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+        var connectionString = connectionStringTemplate
+            .Replace("$MONGODB_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST"))
+            .Replace("$MONGODB_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
+
+        services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+        services.AddScoped<IMongoDatabase>(provider =>
+        {
+            var client = provider.GetRequiredService<IMongoClient>();
+
+            return client.GetDatabase("OrderDatabase");
+        });
+
         return services;
     }
 }
