5516951a59cd3d90af841ce1ae25cfa9b136dd4a
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-14 14:39:57 -0300

    feat: create orders validators

---
+++ Microservices.OrderMicroservice/BusinessLogicLayer/Validators/OrderUpdateRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validators;
+
+public class OrderUpdateRequestValidator : AbstractValidator<OrderUpdateRequest>
+{
+    public OrderUpdateRequestValidator()
+    {
+        RuleFor(temp => temp.OrderId)
+            .NotEmpty()
+            .WithMessage("Order ID can't be blank");
+
+        RuleFor(temp => temp.UserId)
+            .NotEmpty()
+            .WithMessage("User ID can't be blank");
+
+        RuleFor(temp => temp.OrderDate)
+            .NotEmpty()
+            .WithMessage("Order date can't be blank");
+    }
+}
