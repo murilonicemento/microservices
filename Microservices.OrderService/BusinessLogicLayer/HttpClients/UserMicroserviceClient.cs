9538d0910740d6cccda4df3d991b25676ffd13b6
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-16 23:28:54 -0300

    feat: create http client service

---
+++ Microservices.OrderService/BusinessLogicLayer/HttpClients/UserMicroserviceClient.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using BusinessLogicLayer.DTO;
+
+namespace BusinessLogicLayer.HttpClients;
+
+public class UserMicroserviceClient
+{
+    private readonly HttpClient _httpClient;
+
+    public UserMicroserviceClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<User?> GetUserByUserId(Guid userId)
+    {
+        var response = await _httpClient.GetAsync($"/api/user/{userId}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return response.StatusCode switch
+            {
+                HttpStatusCode.NotFound => null,
+                HttpStatusCode.BadRequest => throw new HttpRequestException("Bad request", null,
+                    HttpStatusCode.BadRequest),
+                _ => throw new HttpRequestException("Request failed;")
+            };
+        }
+
+        var user = await response.Content.ReadFromJsonAsync<User>();
+
+        if (user is null)
+            throw new ArgumentException("Invalid user ID.");
+
+        return user;
+    }
+}
