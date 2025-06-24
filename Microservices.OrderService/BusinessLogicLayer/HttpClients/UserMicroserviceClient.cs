3bbf62ccf6150db6a5ae46223de3f1a5cfec659d
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-23 23:16:08 -0300

    feat: add broker circuit exception treatment

--- Microservices.OrderService/BusinessLogicLayer/HttpClients/UserMicroserviceClient.cs
+++ Microservices.OrderService/BusinessLogicLayer/HttpClients/UserMicroserviceClient.cs
@@ -1,29 +1,56 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using BusinessLogicLayer.DTO;
+using Microsoft.Extensions.Logging;
+using Polly.CircuitBreaker;
 
 namespace BusinessLogicLayer.HttpClients;
 
 public class UserMicroserviceClient
 {
     private readonly HttpClient _httpClient;
+    private readonly ILogger<UserMicroserviceClient> _logger;
 
-    public UserMicroserviceClient(HttpClient httpClient)
+    public UserMicroserviceClient(HttpClient httpClient, ILogger<UserMicroserviceClient> logger)
     {
         _httpClient = httpClient;
+        _logger = logger;
     }
 
     public async Task<User?> GetUserByUserId(Guid userId)
     {
-        var response = await _httpClient.GetAsync($"/api/user/{userId}");
-
-        if (!response.IsSuccessStatusCode)
+        try
         {
-            if (response.StatusCode == HttpStatusCode.NotFound)
-                return null;
-            if (response.StatusCode == HttpStatusCode.BadRequest)
-                throw new HttpRequestException("Bad request", null,
-                    HttpStatusCode.BadRequest);
+            var response = await _httpClient.GetAsync($"/api/user/{userId}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                    return null;
+                if (response.StatusCode == HttpStatusCode.BadRequest)
+                    throw new HttpRequestException("Bad request", null,
+                        HttpStatusCode.BadRequest);
+
+                return new User
+                (
+                    PersonName: "Temporarily Unavailable.",
+                    Email: "Temporarily Unavailable.",
+                    Gender: "Temporarily Unavailable.",
+                    UserId: Guid.Empty
+                );
+            }
+
+            var user = await response.Content.ReadFromJsonAsync<User>();
+
+            if (user is null)
+                throw new ArgumentException("Invalid user ID.");
+
+            return user;
+        }
+        catch (BrokenCircuitException exception)
+        {
+            _logger.LogError(exception,
+                "Request failed because of circuit breaker is in Open state. Returning dummy data.");
 
             return new User
             (
@@ -33,12 +60,5 @@ public class UserMicroserviceClient
                 UserId: Guid.Empty
             );
         }
-
-        var user = await response.Content.ReadFromJsonAsync<User>();
-
-        if (user is null)
-            throw new ArgumentException("Invalid user ID.");
-
-        return user;
     }
 }
