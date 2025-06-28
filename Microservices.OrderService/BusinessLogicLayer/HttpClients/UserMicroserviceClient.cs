b63d7f094f6f57a7b3dd9207e1833cf47ee34b11
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-28 20:31:55 -0300

    feat: add caching tu user client

--- Microservices.OrderService/BusinessLogicLayer/HttpClients/UserMicroserviceClient.cs
+++ Microservices.OrderService/BusinessLogicLayer/HttpClients/UserMicroserviceClient.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 using BusinessLogicLayer.DTO;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Polly.CircuitBreaker;
 using Polly.Timeout;
@@ -11,21 +13,40 @@ public class UserMicroserviceClient
 {
     private readonly HttpClient _httpClient;
     private readonly ILogger<UserMicroserviceClient> _logger;
+    private readonly IDistributedCache _distributedCache;
 
-    public UserMicroserviceClient(HttpClient httpClient, ILogger<UserMicroserviceClient> logger)
+    public UserMicroserviceClient(HttpClient httpClient, ILogger<UserMicroserviceClient> logger,
+        IDistributedCache distributedCache)
     {
         _httpClient = httpClient;
         _logger = logger;
+        _distributedCache = distributedCache;
     }
 
     public async Task<User?> GetUserByUserId(Guid userId)
     {
         try
         {
+            var cacheKey = $"user:{userId}";
+            var cachedUser = await _distributedCache.GetAsync(cacheKey);
+
+            if (cachedUser is not null)
+                return JsonSerializer.Deserialize<User>(cachedUser);
+
             var response = await _httpClient.GetAsync($"/api/user/{userId}");
 
             if (!response.IsSuccessStatusCode)
             {
+                if (response.StatusCode == HttpStatusCode.ServiceUnavailable)
+                {
+                    var userFallback = await response.Content.ReadFromJsonAsync<User>();
+
+                    if (userFallback is null)
+                        throw new NotImplementedException();
+
+                    return userFallback;
+                }
+
                 if (response.StatusCode == HttpStatusCode.NotFound)
                     return null;
                 if (response.StatusCode == HttpStatusCode.BadRequest)
@@ -45,6 +66,13 @@ public class UserMicroserviceClient
 
             if (user is null)
                 throw new ArgumentException("Invalid user ID.");
+
+            var serializedUser = JsonSerializer.SerializeToUtf8Bytes(user);
+            var distributedCacheEntryOptions = new DistributedCacheEntryOptions()
+                .SetAbsoluteExpiration(DateTimeOffset.UtcNow.AddMinutes(5))
+                .SetSlidingExpiration(TimeSpan.FromMinutes(3));
+
+            await _distributedCache.SetAsync($"user:{userId}", serializedUser, distributedCacheEntryOptions);
 
             return user;
         }
