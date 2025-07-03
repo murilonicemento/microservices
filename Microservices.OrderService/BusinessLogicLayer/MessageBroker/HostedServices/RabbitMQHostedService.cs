72c4463ed44d41470d95d5d8ac8fbfb2c34bbf24
murilonicemento <murilo_nascimento22@outlook.com>
2025-07-02 23:32:23 -0300

    feat: create hosted service to product update

---
+++ Microservices.OrderService/BusinessLogicLayer/MessageBroker/HostedServices/RabbitMQHostedService.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogicLayer.MessageBroker.Contracts;
+using Microsoft.Extensions.Hosting;
+
+namespace BusinessLogicLayer.MessageBroker.HostedServices;
+
+public class RabbitMQHostedService : IHostedService
+{
+    private readonly IMessageConsumer _productNameUpdateConsumer;
+
+    public RabbitMQHostedService(IMessageConsumer productNameUpdateConsumer)
+    {
+        _productNameUpdateConsumer = productNameUpdateConsumer;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _productNameUpdateConsumer.ConsumeAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _productNameUpdateConsumer.DisposeAsync();
+    }
+}
